b6a08dbeec6ea6d462bde09caebfd0d0114451df
Dennis Nichols <dgnichols22@gmail.com>
2023-03-07 12:58:42 -0500

    Refactor: made function to get place id list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 import os
 import requests
 from dotenv import load_dotenv
@@ -7,6 +8,7 @@ import pandas as pd
 import time
 import json
 import io
+from functions import get_place_ids
 
 load_dotenv()
 key = os.getenv("PLACES_KEY")
@@ -26,42 +28,8 @@ app.add_middleware(
 @app.get("/")
 async def root(city: str = None, business_type: str = None):
   
-  df_list = []  # Initialize an empty list to store dataframes
+  place_id_list = get_place_ids(city=city, business_type=business_type)
   
-  query = f"{business_type} in {city}"
-  search_url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={key}"
-  response_1 = requests.get(search_url).json()
-
-
-  df_1 = pd.json_normalize(response_1["results"])
-  df_list.append(df_1)  # Append df_2 to df_list
-  
-
-  if response_1.get("next_page_token"):
-    token = response_1["next_page_token"]
-    next_url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken={token}&key={key}"
-    time.sleep(2)
-    response_2 = requests.get(next_url).json()
-    # Convert response_2 to a dataframe
-    df_2 = pd.json_normalize(response_2["results"])
-    df_list.append(df_2)  # Append df_2 to df_list
-    
-    if response_2.get("next_page_token"):
-      token = response_2["next_page_token"]
-      next_url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken={token}&key={key}"
-      time.sleep(2)
-      response_3 = requests.get(next_url).json()
-      # Convert response_3 to a dataframe
-      df_3 = pd.json_normalize(response_3["results"])
-      df_list.append(df_3)  # Append df_3 to df_list
-
-  # Concatenate all dataframes in df_list together
-  result_df = pd.concat(df_list, ignore_index=True)
-  # filter to only businesses with at least 100 ratings
-  filtered_df = result_df.query("user_ratings_total > 100")
-  
-  # get list of place ids to make API calls for more details
-  place_id_list = filtered_df['place_id'].tolist()
   fields_list = ['name', 'formatted_address', 'website', 'formatted_phone_number', 'rating', 'user_ratings_total','url']
   fields = '%2C'.join(fields_list)
   
@@ -81,7 +49,15 @@ async def root(city: str = None, business_type: str = None):
   column_values = {}
   for column in data_dict:
       column_values[column] = list(data_dict[column].values())
-
+  for index, address in enumerate(column_values['formatted_address']):
+      column_values['formatted_address'][index] = address.replace(",","").replace("#", "No. ")
+  
   # send the new dictionary to the front end
-  return column_values
+   # add the CORS headers to the response
+  response = JSONResponse(content=column_values)
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, content-type"
+
+  return response
  
